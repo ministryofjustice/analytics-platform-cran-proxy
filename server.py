94b72812476d730f85a52813d4f6206edf53efef
Ravi Kotecha <kotecha.ravi@gmail.com>
2018-11-20 15:15:59 +0000

    BUGFIX: Support packages in the Archive
    We were only compiling current packages but also compiling those in the archive
    will help too.

--- server.py
+++ server.py
@@ -87,14 +87,16 @@ async def packages(request):
 
 
 @app.route("/src/contrib/<package>.tar.gz")
-async def serve_tarfile(request, package):
+@app.route("/src/contrib/<path:path>/<package>.tar.gz")
+async def serve_tarfile(request, package, path = None):
 
     binary_path = app.config.BINARY_OUTPUT_PATH / f"{package}_R_x86_64-pc-linux-gnu.tar.gz"
     if os.path.isfile(binary_path):
         return await file(binary_path)
 
     cran = URLObject(app.config.UPSTREAM_CRAN_SERVER_URL)
-    to = cran.add_path(f"/src/contrib/{package}.tar.gz")
+
+    to = cran.add_path(request.path)
     asyncio.ensure_future(add_to_cache(to))
     logger.info(f"serve_tarfile: Redirecting [302] to {to}")
     return redirect(to)
